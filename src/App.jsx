c38372edfed8cf88ea13e83a41568946ed3a40ff
LaQuangVinh <zlatanvinh2k4@gmail.com>
2023-12-08 22:39:29 +0700

    light dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import ModeSelect from './ModeSelect'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeSelect />
+      <ModeToggle />
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Hello world</Button>
       <Button variant="outline">Hello world</Button>
---
+++ src/ModeSelect.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { useColorScheme } from '@mui/material/styles'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import Box from '@mui/material/Box'
+
+export default function ModeSelect() {
+  // const [age, setAge] = React.useState('')
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="light-dark-mode-label">Mode</InputLabel>
+      <Select
+        labelId="light-dark-mode-label"
+        id="light-dark-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small' />Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <DarkModeIcon fontSize='small' />Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize='small' />System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
