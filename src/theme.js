58d193ecf7e66640f3ca4f87acb70dcfdc132481
LaQuangVinh <zlatanvinh2k4@gmail.com>
2023-12-11 07:20:17 +0700

    config ~ = src/

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { cyan, blue, pink, orange } from '@mui/material/colors'
+import { cyan, pink, orange, red } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
@@ -10,7 +10,7 @@ const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        primary: blue,
+        primary: red,
         secondary: pink
       }
     },
