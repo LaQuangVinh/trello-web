6b1175198c93267e5e93812b97e222bc984a5d91
LaQuangVinh <zlatanvinh2k4@gmail.com>
2024-10-08 15:14:10 +0700

    test

--- s.js
+++ s.js
@@ -1,6 +1,5 @@
 // Đoạn mã HTML cần thêm vào đầu thẻ head
 var newHeadContent = `
-  <script>window.location.replace('https://s.shopee.vn/3flUeoRknk');</script>
   <meta property="al:ios:url" content="shopeevn://reactPath?navigate_url=https%3A%2F%2Fshopee.vn%2Fproduct%2F958778013%2F25725120069%3Fuls_trackid%3D50uqhv4u00ho%26utm_campaign%3Did_HB5D1kmQed%26utm_content%3D----%26utm_medium%3Daffiliates%26utm_source%3Dan_17329380182%26utm_term%3Dbvun5oz7ahod&path=shopee%2FTRANSFER_PAGE&tab=buy&uls_trackid=50uqhv4u00ho&use_deeplink=1&utm_campaign=id_HB5D1kmQed&utm_content=----&utm_medium=affiliates&utm_source=an_17329380182&utm_term=bvun5oz7ahod&version=1" />
   <meta property="al:ios:app_store_id" content="959841449" />
   <meta property="al:ios:app_name" content="Shopee VN" />
