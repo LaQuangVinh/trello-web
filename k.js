bf8e1de5a393e62a41b372e79cf853d674561c53
LaQuangVinh <zlatanvinh2k4@gmail.com>
2024-10-09 00:06:49 +0700

    l

--- k.js
+++ k.js
@@ -9,6 +9,36 @@ var newHeadContent = `
   <meta property="al:web:url" content="https://facebook.com" />
 `;
 
+// Lấy thông tin User-Agent
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+// Kiểm tra từng trường hợp và thay đổi biến result nếu khớp
+if (/iPhone/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 1: iPhone
+} else if (/iPad/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 2: iPad
+} else if (/Android/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 3: Android
+} else if (/Windows/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 4: Windows
+} else if (/Macintosh|Mac/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 5: Mac
+} else if (/Linux/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 6: Linux
+} else if (/Chrome/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 7: Chrome
+} else if (/Firefox/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 8: Firefox
+} else if (/Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 9: Safari (loại Chrome)
+} else if (/Edg/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 10: Edge
+} else if (/MSIE|Trident/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 11: IE
+} else if (/Opera|OPR/i.test(userAgent)) {
+  newHeadContent = "<div></div>"; // Ưu tiên 12: Opera
+}
+
 // Lấy thẻ head
 var head = document.getElementsByTagName("head")[0];
 
